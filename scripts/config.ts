0d24a3b2b392802ae49b39eb26480fc4cf62795f
hendrash <hendrash@mail.gvsu.edu>
2021-06-13 12:49:22 -0400

    generate

--- scripts/config.ts
+++ scripts/config.ts
@@ -3,8 +3,8 @@ import { HttpProviderOptions, WebSocketEvent, WebsocketProviderOptions } from "w
 import { networkConfig } from "./globals";
 import BridgeEvent from "../build/abi/TransferToUSDC.json";
 export const config=()=>{require('dotenv').config()}
-const httpProvider = new Web3.providers.HttpProvider(networkConfig().RPC_URL, {timeout:10000} as HttpProviderOptions)
-const sockProvider = new Web3.providers.WebsocketProvider(networkConfig().socket,{timeout:10000} as WebsocketProviderOptions)
+export const httpProvider = new Web3.providers.HttpProvider(networkConfig().RPC_URL, {timeout:10000} as HttpProviderOptions)
+export const sockProvider = new Web3.providers.WebsocketProvider(networkConfig().socket,{timeout:10000} as WebsocketProviderOptions)
 export const web3Provider=new Web3(httpProvider);
 export const webSocketProvider=new Web3(sockProvider);
 export const bridgeEvent = new web3Provider.eth.Contract((BridgeEvent as any), networkConfig().TransferToUSDC)
